2f2d8dc5a721fa2cd1288735f8aae8761cf38a33
TarasCoder <66311545+TarasCoder@users.noreply.github.com>
2021-10-30 12:19:14 +0300

    Update List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,55 +1,14 @@
-import React, { useState } from "react";
-import List from "./List";
+import React from "react";
 
-function App() {
-  const [inputText, setInputText] = useState("");
-  const [items, setItems] = useState([]);
-
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setInputText(newValue);
-  }
-
-  function addItem(ev) {
-    ev.preventDefault();
-    if (inputText) {
-      setItems((prevItems) => {
-        return [...prevItems, inputText];
-      });
-      setInputText("");
-    } else alert("Enter something!");
-  }
-
-  function deleteItem(id) {
-    setItems((prev) => {
-      return prev.filter((item, index) => {
-        return index !== id;
-      });
-    });
-  }
-
+function List(props) {
   return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-        <form>
-          <input onChange={handleChange} type="text" value={inputText} />
-          <button onClick={addItem}>
-            <span>Add</span>
-          </button>
-        </form>
-      </div>
-      <div>
-        <ul>
-          {items.map((todoItem, index) => (
-            <List key={index} id={index} text={todoItem} onCheck={deleteItem} />
-          ))}
-        </ul>
-      </div>
+    <div
+      onClick={() => {
+        props.onCheck(props.id);
+      }}
+    >
+      <li>{props.text}</li>
     </div>
   );
 }
-
-export default App;
+export default List;
